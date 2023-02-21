0e95574d5b56e737b45ea54a1967d5df9b033fdd
BikashMishraDev <bikash@solultima.com>
2023-02-21 14:31:51 +0530

    getproductbyid

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 import { Request, Response } from 'express';
-import { getList, createProduct, getProductByID } from './products';
+import {  getProductByID } from './products';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/products', getList);
 app.get('/products/:id', getProductByID);
-app.post('/products', createProduct);
+
 app.use('/**', (req, res) => {
     res.status(404).send({error: 'resource not found'});
 })
--- src/products/get.ts
+++ src/products/get.ts
@@ -1,14 +1,27 @@
 import { Request, Response } from 'express';
+import * as fs from 'fs';
+import { productModel } from './product.model';
 
-const getList = (req: Request, res: Response) => {
-    res.send([]);
-}
+const dataFileName = './database/products.json';
+const dataFile = dataFileName;
+
+
+let products : productModel[] =JSON.parse(fs.readFileSync(dataFile).toString())
+
+const findOneOfID = (id: String): productModel => {
+    const toBeFound: productModel = products.find(t => t.id === id)!;
+    return toBeFound;
+  }
+
+
 
 const getProductByID = (req: Request, res: Response) => {
-    res.send({ id: req.params.id });
+    const id = req.params.id;
+    let targetid = findOneOfID(id);
+    res.json({targetid })
 }
 
 export {
-    getList,
+   
     getProductByID
 }
