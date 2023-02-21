f1e4ee5faf82db98db67882fcc972ec674b81212
harshithakanala31 <harshitha@solultima.com>
2023-02-21 14:54:48 +0530

    code for create product

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 import { Request, Response } from 'express';
 import { getList, createProduct, getProductByID } from './products';
@@ -5,6 +6,7 @@ import { getList, createProduct, getProductByID } from './products';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
 app.get('/products', getList);
 app.get('/products/:id', getProductByID);
 app.post('/products', createProduct);
--- src/products/post.ts
+++ src/products/post.ts
@@ -1,8 +1,14 @@
+import bodyParser from 'body-parser';
 import { Request, Response } from 'express';
 import { createItem } from './database';
 const createProduct = (req: Request, res: Response) => {
-    const newID = createItem('product 3', 23, 23);
-    res.send({ id: newID });
+    console.log(req.body);
+    Object.keys(req.headers).forEach(key => console.log(`${key} => ${req.headers[key]}`));
+    Object.keys(req.query).forEach(key => console.log(`${key} => ${req.query[key]}`));
+
+    var urlencodedParser = bodyParser.urlencoded({ extended: false })
+     const newID = createItem(' req.body.name', req.body.price, req.body.stock);
+     res.send({ id: newID });
 }
 
 export {
