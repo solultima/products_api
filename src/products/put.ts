7b9f24a7b18a898d0a5b4935fcb17b5f27d615e1
Anant Anand Gupta <aag@solultima.com>
2023-02-23 11:39:38 +0530

    cleaned

--- src/products/put.ts
+++ src/products/put.ts
@@ -6,15 +6,11 @@ import { validate } from 'uuid';
 export const updateStockHandler = (req: Request, res: Response) => {
   const id = req.params.id;
   const stock = req.body.stock;
-  if (id && validate(id) && stock && typeof stock === 'number' && stock >= 0) {
-    const updatedProduct = updateStock(id, stock);
-    if (updatedProduct) {
-      res.send(updatedProduct);
-    } else {
-      res.status(404).send({ error: 'not able to update product' });
-    }
+  const updatedProduct = updateStock(id, stock);
+  if (updatedProduct) {
+    res.send(updatedProduct);
   } else {
-    res.status(400).send({ error: 'either id or stock missing or invalid' });
+    res.status(404).send({ error: 'not able to update product' });
   }
 };
 
---
+++ src/products/validators.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from 'express';
+import { validate } from 'uuid';
+
+export const updateStockValidator = (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    const stock = req.body.stock;
+    if (id && validate(id) && stock && typeof stock === 'number' && stock >= 0) {
+        console.log(JSON.stringify(req.body));
+        return next();
+    } else {
+        res.status(400).send({ error: 'either id or stock missing or invalid' });
+    }
+}
